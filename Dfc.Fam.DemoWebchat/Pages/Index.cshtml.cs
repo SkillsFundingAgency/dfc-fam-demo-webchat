44c7b9bd82c1d0aff3d9617dbe7259caeb87f8ee
Ian Crisp <icrisp@program-systems.co.uk>
2020-01-21 16:06:09 +0000

    Initial development commit

--- Dfc.Fam.DemoWebchat/Pages/Index.cshtml.cs
+++ Dfc.Fam.DemoWebchat/Pages/Index.cshtml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace Dfc.Fam.DemoWebChat.Pages
@@ -15,11 +10,6 @@ namespace Dfc.Fam.DemoWebChat.Pages
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
         }
     }
 }
